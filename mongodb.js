4d262cf1929d2aceb8f9d161f3acdf4608ab8fac
DESKTOP-3OTLM8O\Mata <mperkovic@protonmail.com>
2022-01-31 20:14:15 +0100

    add mongodb

---
+++ mongodb.js
@@ -0,0 +1,16 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-app';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) return console.log(error);
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'Matija',
+        age: 24
+    });
+});
